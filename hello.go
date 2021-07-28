e051fbba358ab6aaf184e5424c428d196ee7c450
Ulitin Ivan <47325255+GiaSoPas@users.noreply.github.com>
2021-07-28 16:01:20 +0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -9,7 +9,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/hello", func(c *gin.Context) {
-		c.String(200, "Hello, Worldd!sheeshhh")
+		c.String(200, "Hello, Worldd!sheewshhh")
 	})
 
 	api := r.Group("/api")
